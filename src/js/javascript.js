728f25b8574788ab623e86730dd680256481d087
Kammet07 <pavel.pupentsov@educanet.cz>
2019-05-05 21:09:44 +0200

    Pavel19.2 hra1

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -36,13 +36,40 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function hra() {
-        console.log('hra');
-        $('#content').text("loading");
-        $('#content').load('hra.html');
-        
-        $(document).ready(function () {
-            $('.tabs').tabs();
+        $('#content').load('hra.html', function (responseTxt, statusTxt, xhr) {
+            if (statusTxt == "success") {
+                $(document).ready(function () {
+                    $('.tabs').tabs();
+                });
+
+                // const form = document.getElementById('add-requests-form');
+
+                // form.addEventListener('submit', (e) => {
+
+                //     e.preventDefault();
+
+                //     db.collection('requests').add({
+                //         otazka: form.otazka.value,
+                //         spravnaOdpoved: form.spravnaOdpoved.value,
+                //         spatnaOdpoved1: form.spatnaOdpoved1.value,
+                //         spatnaOdpoved2: form.spatnaOdpoved2.value,
+                //         spatnaOdpoved3: form.spatnaOdpoved3.value
+                //     });
+                //     form.otazka.value = '';
+                //     form.spravnaOdpoved.value = '';
+                //     form.spatnaOdpoved1.value = '';
+                //     form.spatnaOdpoved2.value = '';
+                //     form.spatnaOdpoved3.value = '';
+                // });
+            }
         });
+        // console.log('hra');
+        // $('#content').text("loading");
+        // $('#content').load('hra.html');
+        
+        // $(document).ready(function () {
+        //     $('.tabs').tabs();
+        // });
     }
 
     // db.collection('Requests').orderBy('name').onSnapshot(snapshot => {
